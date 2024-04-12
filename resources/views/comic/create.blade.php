5a2e82257e90bb585a6056b1a4fbe11e9656c2e9
vi_ser <vinceulisse@gmail.com>
2024-04-12 10:33:27 +0200

    fixed: input type date per la data di pubblicazione del form

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -36,7 +36,7 @@
             </div>
             <div class="mb-3">
               <label for="sale_date" class="form-label">Data di vendita</label>
-              <input type="text" class="form-control" id="sale_date" name="sale_date">
+              <input type="date" class="form-control" id="sale_date" name="sale_date">
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo</label>
